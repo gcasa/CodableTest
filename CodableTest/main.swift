9fb6c223fe3d9b45d34455c2fa05a38b783b9a2e
Gregory Casamento <greg.casamento@gmail.com>
2024-05-07 23:22:18 -0400

    Add all

--- CodableTest/main.swift
+++ CodableTest/main.swift
@@ -7,5 +7,20 @@
 
 import Foundation
 
-print("Hello, World!")
+struct Person : Codable {
+    var name : String
+    var age : Int
+}
 
+var person = Person(name: "Greg", age: 54)
+var encoder = JSONEncoder()
+var data : Data
+
+do {
+    try data = encoder.encode(person)
+    let str = String(decoding: data, as: UTF8.self)
+    print(str)
+}
+catch {
+    print("Error encoding")
+}
